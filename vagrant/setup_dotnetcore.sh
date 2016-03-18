a412da1d7e57efc3f34f4207552611e361fe6f8e
CJ Taylor <cj-taylor@users.noreply.github.com>
2016-03-18 10:23:02 -0400

    Followup to #1 to fix mono version causing hanging request.

--- vagrant/setup_dotnetcore.sh
+++ vagrant/setup_dotnetcore.sh
@@ -16,7 +16,11 @@
 # 3. To install DNX for Mono:
     # 1. Install Mono via the mono-complete package.
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+    
+    # mono source - v4.2 has some bugs, downgrading per https://github.com/cj-taylor/monyx/issues/1
+    # echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+    echo "deb http://download.mono-project.com/repo/debian wheezy/snapshots/4.0.5.1/. main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+    
     sudo apt-get update
     sudo apt-get install -y mono-complete
     
